d37bb2d1616b0f58dddb0295f919b57cb19c9924
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-22 15:25:24 +0200

    implementation_customers

--- src/main/java/team3/epic_energy_services/entities/Cliente.java
+++ src/main/java/team3/epic_energy_services/entities/Cliente.java
@@ -1,4 +1,36 @@
 package team3.epic_energy_services.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "customers")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(value = AccessLevel.NONE)
+    private UUID id;
+    @Enumerated(EnumType.STRING)
+    private TypeCustomers ragioneSociale;
+    private int partitaIva;
+    private String email;
+    private LocalDate dataInserimento;
+    private LocalDate dataUltimoContatto;
+    private double fatturatoAnnuale;
+    private String pec;
+    private int telefono;
+    private String emailContatto;
+    private String nomeContatto;
+    private String cognomeContatto;
+    private String telefonoContatto;
+    private String logo_aziendale;
 }
