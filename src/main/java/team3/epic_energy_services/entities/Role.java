a282b4d77220332a14513615216393ac7ebeae0e
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-22 14:33:46 +0200

    Created Role entity

--- src/main/java/team3/epic_energy_services/entities/Role.java
+++ src/main/java/team3/epic_energy_services/entities/Role.java
@@ -1,4 +1,20 @@
 package team3.epic_energy_services.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(value = AccessLevel.NONE)
+    private UUID id;
+    private String role;
 }
