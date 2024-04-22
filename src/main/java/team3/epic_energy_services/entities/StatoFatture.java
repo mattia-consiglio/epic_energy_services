225e7eeb7cf0e86e87d287a1bffeb7dcffc56742
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-04-22 15:04:34 +0200

    implementazione delle entità Fattura e StatoFattura

---
+++ src/main/java/team3/epic_energy_services/entities/StatoFatture.java
@@ -0,0 +1,19 @@
+package team3.epic_energy_services.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Stato_fattura")
+public class StatoFatture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String stato;
+}
