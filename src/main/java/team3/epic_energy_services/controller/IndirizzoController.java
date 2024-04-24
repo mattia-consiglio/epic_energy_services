b44571f6cc5e8942fc085111a4f2697a568b4110
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-24 17:09:35 +0200

    complete_crud

--- src/main/java/team3/epic_energy_services/controller/IndirizzoController.java
+++ src/main/java/team3/epic_energy_services/controller/IndirizzoController.java
@@ -3,6 +3,7 @@ package team3.epic_energy_services.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +34,7 @@ public class IndirizzoController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     private Indirizzo saveNewIndirizzo(@RequestBody @Validated IndirizzoDTO payload, BindingResult validation) {
         if (validation.hasErrors()) throw new BadRequestException("invlaid data", validation.getAllErrors());
@@ -40,12 +42,14 @@ public class IndirizzoController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     private void deleteIndirizzo(@PathVariable UUID id) {
         iS.findByIdAndDelete(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     private Indirizzo updateDevice(@PathVariable UUID id, @RequestBody @Validated IndirizzoDTO payload, BindingResult validation) {
         if (validation.hasErrors()) throw new BadRequestException("invlaid data", validation.getAllErrors());
         else return iS.findByIdAndUpdate(id, payload);
--- src/main/java/team3/epic_energy_services/payloads/IndirizzoDTO.java
+++ src/main/java/team3/epic_energy_services/payloads/IndirizzoDTO.java
@@ -12,7 +12,7 @@ public record IndirizzoDTO(
         String via,
 
         @NotEmpty(message = "devi mettere un numero civico")
-        @Size(min = 2, max = 40, message = "il numero civico deve contenere da un minimo di 2 caratteri and un massimo di 40")
+        @Size(min = 1, max = 400, message = "il numero civico deve contenere da un minimo di 1 caratteri and un massimo di 400")
         String civico,
 
         @NotEmpty(message = "devi mettere una località")
@@ -20,10 +20,10 @@ public record IndirizzoDTO(
         String localita,
 
         @NotEmpty(message = "devi mettere un cap")
-        @Size(min = 2, max = 40, message = "il cap deve contenere da un minimo di 2 caratteri and un massimo di 40")
+        @Size(min = 5, max = 5, message = "il cap deve contenere 5 caratteri")
         String cap,
 
-        @NotNull(message = "devi mettere un comune")
+        @NotNull(message = "devi mettere un comune tramite UUID")
         UUID comune
         ) {
 }
